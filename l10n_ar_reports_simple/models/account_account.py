5a7b4b9b9cf753e4a777c8151ada10974cb14b14
Frankofe <frankofe1997@gmail.com>
2026-02-26 08:56:27 -0300

    add-l10n_ar_reports_simple

---
+++ l10n_ar_reports_simple/models/account_account.py
@@ -0,0 +1,22 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import fields, models
+
+
+class AccountAccount(models.Model):
+    _inherit = 'account.account'
+
+    fiscal_code = fields.Char(
+        string='Código fiscal',
+        related='company_id.account_fiscal_country_id.code',
+        store=True,
+        readonly=True,
+    )
+    l10n_ar_arca_activity_id = fields.Many2one(
+        'l10n_ar.arca.activity',
+        string='Actividad ARCA asociada',
+        help=(
+            "Argentina: Este campo sirve para asociar una actividad específica a utilizar con esta cuenta."
+            "Si no se establece, se utilizará la actividad predeterminada de la compañía."
+            "La actividad se usará al generar los informes de IVA ARCA."
+        ),
+    )
